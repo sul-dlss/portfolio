35351dff18e57d1a79a6d8f7ef3970a046aa7f0d
Chris Beer <cabeer@stanford.edu>
2016-07-07 12:10:24 -0700

    Bump capistrano lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.4
-lock '3.4.0'
+# config valid only for Capistrano 3.5
+lock '3.5.0'
 
 set :application, 'eportfolios'
 set :repo_url, 'https://github.com/sul-dlss/portfolios.git'
